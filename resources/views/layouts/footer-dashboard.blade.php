933cfb4cc746669ec2e8df32caddfd8c13359e7b
Fauzaro01 <muhamadfauzan4750@gmail.com>
2025-02-10 21:52:09 +0700

    Add : school name to footer

--- resources/views/layouts/footer-dashboard.blade.php
+++ resources/views/layouts/footer-dashboard.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="footer clearfix mb-0 text-muted">
         <div class="float-start">
-            <p>{{date('Y')}} &copy; PT. Bintang Mas Karya Nusantara</p>
+            <p>{{date('Y')}} &copy; PT Bintang Mas Karya Nusantara <br> SMKS TI Muhammadiyah 1 Cikampek. </p>
         </div>
         <div class="float-end">
             <p>Pages Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
